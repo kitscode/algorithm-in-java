bb9cd8d7ba61d7fc1d809c1db9a0372c2433317c
kitscode <aerockevin@gmail.com>
2019-03-04 18:39:10 +0800

    number of 1 analysis added

--- src/main/java/com/kevll/blade/Q11_NumberOf1.java
+++ src/main/java/com/kevll/blade/Q11_NumberOf1.java
@@ -1,5 +1,7 @@
 package com.kevll.blade;
 
+import org.junit.Test;
+
 /**
  * @author: Kevin Zhang
  * @date: 2018/12/12
@@ -13,7 +15,7 @@ public class Q11_NumberOf1 {
         int count = 0;
         while (n != 0) {
             count++;
-            n = n & (n - 1);
+            n = n & (n - 1);    // n-1把最右边的1变为0，之后的0都变为1，相与后抵消。所以每次n与n-1做与运算后，1的个数会减少一个
         }
         return count;
     }
@@ -28,5 +30,14 @@ public class Q11_NumberOf1 {
 
         System.out.println(numberOf1Java(1));
         System.out.println(numberOf1Java(-1));
+    }
+
+    @Test
+    public void test() {
+        System.out.println(Integer.toBinaryString(1));
+        System.out.println(Integer.toBinaryString(-1));
+        System.out.println(Integer.toBinaryString(-1).length());
+
+        System.out.println(Integer.toBinaryString(-1 - 1));    // 负数也是抵消
     }
 }
